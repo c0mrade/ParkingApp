ae62ed60c187cee9f7cb19f48a31aa855bdf686d
c0mrade <emir.ibrahimbegovic@gmail.com>
2019-02-01 05:46:16 -0500

    Adjust query accordingly

--- app/helpers/parking_transaction_helper.rb
+++ app/helpers/parking_transaction_helper.rb
@@ -1,10 +1,12 @@
 module ParkingTransactionHelper
+  # select all that have less occupancy than number of spaces also
+  # select who have no occupancy, hence the left join
   def available_floors_with_parking_spaces
     Floor.where(id:
       Floor
-        .select('count(parking_transactions.id) as cnt', 'floor_id', 'number_of_spaces')
-        .joins(:parking_transactions)
-        .where('parking_transactions.active = ?', true)
+        .select('count(parking_transactions.id) as cnt', 'floors.id as floor_id', 'number_of_spaces')
+        .joins('left join parking_transactions on parking_transactions.floor_id = floors.id')
+        .where('parking_transactions.active = ? or parking_transactions.id is null', true)
         .group('floor_id, number_of_spaces')
         .having('cnt < number_of_spaces')
         .collect(&:floor_id)
